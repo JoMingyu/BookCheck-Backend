34d87e7767a4bfe3719642bd0e3cb24ca0c4d70b
PlanB <city7310@naver.com>
2017-12-05 10:25:38 +0900

    [DONE] Mongo, ViewInjector

---
+++ Server/app/views/__init__.py
@@ -0,0 +1,15 @@
+from flask_restful import Api
+from flasgger import Swagger
+
+from app.docs import TEMPLATE
+
+
+class ViewInjector(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        Swagger(app, template=TEMPLATE)
+
+        api = Api(app)
