97ded830efb38dbf5ffcef119fd00d645b55d26f
PlanB <city7310@naver.com>
2017-12-05 10:27:55 +0900

    [DONE] Auth APIs

--- Server/app/views/user.py
+++ Server/app/views/user.py
@@ -25,3 +25,46 @@ class Signup(Resource):
 
         return Response('', 201)
 
+
+class AuthCommonUser(Resource):
+    def post(self):
+        """
+        일반 사용자 로그인
+        """
+        id: str = request.form['id']
+        pw: str = request.form.get['pw']
+
+        user = UserModel.objects(id=id, pw=pw).first()
+
+        if not user:
+            return Response('', 401)
+
+        refresh_token = uuid4()
+        RefreshTokenModel(refresh_token=refresh_token, owner=user, pw_snapshot=pw).save()
+
+        return {
+            'access_token': create_access_token(id),
+            'refresh_token': create_refresh_token(str(refresh_token))
+        }, 200
+
+
+class AuthAdmin(Resource):
+    def post(self):
+        """
+        관리자 로그인
+        """
+        id: str = request.form['id']
+        pw: str = request.form.get['pw']
+
+        admin = AdminModel.objects(id=id, pw=pw).first()
+
+        if not admin:
+            return Response('', 401)
+
+        refresh_token = uuid4()
+        RefreshTokenModel(refresh_token=refresh_token, owner=admin, pw_snapshot=pw).save()
+
+        return {
+            'access_token': create_access_token(id),
+            'refresh_token': create_refresh_token(str(refresh_token))
+        }, 200
