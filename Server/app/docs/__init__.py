0bbdd75a922ff0c72b8f01e30456fb4416835f0a
PlanB <city7310@naver.com>
2017-12-06 08:51:19 +0900

    [ADD] Library, Book, Borrow Swagger Templates

--- Server/app/docs/__init__.py
+++ Server/app/docs/__init__.py
@@ -4,8 +4,20 @@ TEMPLATE = {
     ],
     'tags': [
         {
-            'name': 'Tag',
-            'description': 'Desc'
+            'name': '계정',
+            'description': '계정 관련 API'
         },
+        {
+            'name': '도서관',
+            'description': '도서관 가입과 탈퇴에 관한 API'
+        },
+        {
+            'name': '책',
+            'description': '도서관의 책 관리에 관한 API'
+        },
+        {
+            'name': '대출',
+            'description': '도서관의 책 대출/반납에 관한 API'
+        }
     ]
 }
